ee8baca086236d24c2503ff16db6396dbbf7b00b
Margouta <56451786+Margouta@users.noreply.github.com>
2024-07-01 19:27:38 +0200

    initial commit

---
+++ src/main/java/fr/communaywen/core/AywenCraftPlugin.java
@@ -0,0 +1,17 @@
+package fr.communaywen.core;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AywenCraftPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getServer().getLogger().info("Hello le monde!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
