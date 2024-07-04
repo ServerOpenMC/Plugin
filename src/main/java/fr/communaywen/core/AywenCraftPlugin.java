57dbc6f73845fae9eeb530d5167abe3042c28f6a
DELILLE Elone <147847949+HiNett@users.noreply.github.com>
2024-07-04 20:12:44 +0200

    Code: Update Core .java
    Update the AywenCraftPlugin.java to start the MOTD Changer

--- src/main/java/fr/communaywen/core/AywenCraftPlugin.java
+++ src/main/java/fr/communaywen/core/AywenCraftPlugin.java
@@ -1,17 +1,27 @@
 package fr.communaywen.core;
 
+import fr.communaywen.core.utils.MOTDChanger;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AywenCraftPlugin extends JavaPlugin {
 
+    private MOTDChanger motdChanger;
+
     @Override
     public void onEnable() {
         Bukkit.getServer().getLogger().info("Hello le monde, ici le plugin AywenCraft !");
+
+        // Init + Launch MOTD
+        motdChanger = new MOTDChanger();
+        motdChanger.onEnable();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        // Disable MOTD
+        if (motdChanger != null) {
+            motdChanger.onDisable();
+        }
     }
 }
