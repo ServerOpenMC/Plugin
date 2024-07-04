f5cfb0cc5fec402df4406a7fcf662b7aba13b9db
misieur <153451816+misieur@users.noreply.github.com>
2024-07-04 16:12:17 +0200

    Create je_ne_sais_pas_quoi_mettre_comme_nom_mais_ca_permet_de_ne_pas_casser_le_champs_quand_on_saute_dessus.java

---
+++ src/main/java/fr/communaywen/core/je_ne_sais_pas_quoi_mettre_comme_nom_mais_ca_permet_de_ne_pas_casser_le_champs_quand_on_saute_dessus.java
@@ -0,0 +1,31 @@
+package fr.communaywen.core;
+
+import org.bukkit.Material;
+
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityInteractEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class Farme implements Listener {
+
+    @EventHandler
+    public void onEntityInteract(EntityInteractEvent event) {
+        Block block = event.getBlock();
+        if(block != null && block.getType() == Material.FARMLAND) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if(event.getAction() == Action.PHYSICAL) {
+            if(event.getClickedBlock().getType() == Material.FARMLAND) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+}
