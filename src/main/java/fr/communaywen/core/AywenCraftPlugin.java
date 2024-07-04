79917e5a6c7ab30872772c0b2c51f7c240766c20
misieur <153451816+misieur@users.noreply.github.com>
2024-07-04 16:10:45 +0200

    Update AywenCraftPlugin.java

--- src/main/java/fr/communaywen/core/AywenCraftPlugin.java
+++ src/main/java/fr/communaywen/core/AywenCraftPlugin.java
@@ -8,6 +8,7 @@ public final class AywenCraftPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         Bukkit.getServer().getLogger().info("Hello le monde, ici le plugin AywenCraft !");
+        getServer().getPluginManager().registerEvents(new je_ne_sais_pas_quoi_mettre_comme_nom_mais_ca_permet_de_ne_pas_casser_le_champs_quand_on_saute_dessusje_ne_sais_pas_quoi_mettre_comme_nom_mais_ca_permet_de_ne_pas_casser_le_champs_quand_on_saute_dessus(),this);
     }
 
     @Override
