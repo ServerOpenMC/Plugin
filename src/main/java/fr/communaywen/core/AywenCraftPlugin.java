a3b3ca962656239c644fe1cad0483589970be20a
Adam CAVILLON <adam@codinbox.fr>
2024-07-04 22:41:30 +0200

    [Code] Ajout d'un préfixe de permission commun

--- src/main/java/fr/communaywen/core/AywenCraftPlugin.java
+++ src/main/java/fr/communaywen/core/AywenCraftPlugin.java
@@ -4,8 +4,16 @@ import fr.communaywen.core.utils.MOTDChanger;
 import fr.communaywen.core.commands.VersionCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public final class AywenCraftPlugin extends JavaPlugin {
+
+    /**
+     * Permission prefix.
+     * <br>
+     * Permissions in the plugin <b>SHOULD</b> be prefixed with this prefix. E.g. <code>ayw.command.prout</code>
+     */
+    public static final @NotNull String PERMISSION_PREFIX = "ayw";
 
     private MOTDChanger motdChanger;
 
@@ -16,10 +24,24 @@ public final class AywenCraftPlugin extends JavaPlugin {
         motdChanger = new MOTDChanger();
         motdChanger.startMOTDChanger(this);
 
+
+
         this.getCommand("version").setExecutor(new VersionCommand(this));
     }
 
     @Override
     public void onDisable() {
     }
+
+    /**
+     * Format a permission with the permission prefix.
+     *
+     * @param suffix The permission suffix.
+     * @return The formatted permission.
+     * @see #PERMISSION_PREFIX
+     */
+    public static @NotNull String formatPermission(final @NotNull String suffix) {
+        return PERMISSION_PREFIX + "." + suffix;
+    }
+
 }
