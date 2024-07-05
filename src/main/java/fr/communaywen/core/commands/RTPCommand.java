96c0bef2b9ee8efd3581e28d79e9949bf3b6952a
misieur <153451816+misieur@users.noreply.github.com>
2024-07-05 14:17:49 +0200

    Update RTPCommand.java

--- src/main/java/fr/communaywen/core/commands/RTPCommand.java
+++ src/main/java/fr/communaywen/core/commands/RTPCommand.java
@@ -1,6 +1,5 @@
 package fr.communaywen.core.commands;
 
-import fr.communaywen.core.AywenCraftPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
@@ -22,13 +21,22 @@ import java.util.UUID;
 public class RTPCommand implements CommandExecutor {
 
 	
-	//Merci à ri1ongithub pour le système de cooldown que j'avais la flème de refaire
-	//Je n'ai pas fait de test mais normalement il y a pas de bug (j'ai quand même testé si l'aléatoir marchait)
-
 	
+
+    private final Configs plugin;
+    public RTPCommand(Congis plugin){
+	    this.plugin = plugin;
+    }
+
+
+    //Merci à ri1ongithub pour le système de cooldown que j'avais la flème de refaire
     private final HashMap<UUID, Long> cooldowns = new HashMap<>();
+    private static final int COOLDOWN = this.plugin.getConfig().getint("rtp.cooldown"); //temps en secondes
+    private static final int COOLDOWN_ERROR = this.plugin.getConfig().getint("rtp.cooldown-error"); //temps en secondes
     
     
+    
+	
 	@Override
     public boolean onCommand(final CommandSender sender,final Command command,final String label,final String[] args) {
         if (sender instanceof Player player) {
@@ -39,22 +47,23 @@ public class RTPCommand implements CommandExecutor {
                 long lastUsed = cooldowns.get(playerId);
                 long timeSinceLastUse = currentTime - lastUsed;
 
-                if (timeSinceLastUse < AywenCraftPlugin.getInstance().getConfig().getint("rtp.cooldown")) {
-                    long timeLeft = AywenCraftPlugin.getInstance().getConfig().getint("rtp.cooldown") - timeSinceLastUse;
+                if (timeSinceLastUse < COOLDOWN_TIME) {
+                    long timeLeft = COOLDOWN_TIME - timeSinceLastUse;
                     player.sendMessage("Vous devez attendre encore " + timeLeft + " secondes avant d'utiliser cette commande à nouveau.");
                     return true;
                 }
             }
-	    
-            int minx = AywenCraftPlugin.getInstance().getConfig().getint("rtp.minx");
-            int maxx = AywenCraftPlugin.getInstance().getConfig().getint("rtp.maxx");
-            int minz = AywenCraftPlugin.getInstance().getConfig().getint("rtp.minz");
-            int maxz = AywenCraftPlugin.getInstance().getConfig().getint("rtp.maxz");
+            int minx = this.plugin.getConfig().getint("rtp.minx");
+            int maxx = this.plugin.getConfig().getint("rtp.maxx");
+            int miny = this.plugin.getConfig().getint("rtp.miny");
+            int maxy = this.plugin.getConfig().getint("rtp.maxy");
+            int minz = this.plugin.getConfig().getint("rtp.minz");
+            int maxz = this.plugin.getConfig().getint("rtp.maxz");
             int x = (int) ((Math.random() * (maxx - minx)) + minx);
             int z = (int) ((Math.random() * (maxz - minz)) + minz);
             World world = player.getWorld();
             Location location,belowlocation;
-            for (int y = 64; y<100;y++) {
+            for (int y = miny; y<maxy;y++) {
             	location = new Location(world,x,y,z);
             	if (location.getBlock().getType().isAir()) {
             		belowlocation = new Location(world,x,y-1,z);
@@ -67,8 +76,7 @@ public class RTPCommand implements CommandExecutor {
             	}
             }
             player.sendTitle(" §cErreur",null);
-	    int cooldownerror = AywenCraftPlugin.getInstance().getConfig().getint("rtp.cooldownerror");
-            cooldowns.put(playerId, currentTime - AywenCraftPlugin.getInstance().getConfig().getint("rtp.cooldown") + cooldownerror);
+            cooldowns.put(playerId, currentTime-COOLDOWN_TIME+COOLDOWN_ERROR); //5 secondes de cooldown
             return true;
             
         }
