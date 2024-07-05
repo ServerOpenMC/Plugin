faeec54f37d53822e5f36dd9aef061db4edefdc7
MathiasDPX <mathias@dupeux.net>
2024-07-05 20:46:38 +0200

    [Command] Add CreditCommand

---
+++ src/main/java/fr/communaywen/core/commands/CreditCommand.java
@@ -0,0 +1,42 @@
+package fr.communaywen.core.commands;
+
+import fr.communaywen.core.utils.GitHubAPI;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class CreditCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            GitHubAPI gitHubAPI = new GitHubAPI();
+            try {
+                String jsonResponse = gitHubAPI.getContributors();
+                if (jsonResponse.startsWith("Error:")) {
+                    player.sendMessage("Impossible de récuperer les contributeurs: " + jsonResponse);
+                } else {
+                    JSONArray contributors = new JSONArray(jsonResponse);
+                    player.sendMessage("§lMerci à§r");
+                    for (int i = 0; i < contributors.length(); i++) {
+                        JSONObject contributor = contributors.getJSONObject(i);
+                        String login = contributor.getString("login");
+                        player.sendMessage("- §6" + login + "§r");
+                    }
+                }
+            } catch (IOException e) {
+                player.sendMessage("Une erreur est apparue lors de la récuperation des contributeurs.");
+                e.printStackTrace();
+            }
+        } else {
+            sender.sendMessage("Cette commande peut seulement être éxécutée par un joueur.");
+        }
+        return true;
+    }
+}
