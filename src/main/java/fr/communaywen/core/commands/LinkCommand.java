ad46c84fd438261391c6aca0263b4f1f1cf34e93
MathiasDPX <56231137+MathiasDPX@users.noreply.github.com>
2024-07-06 13:18:12 +0200

    Refactor import header

--- src/main/java/fr/communaywen/core/commands/LinkCommand.java
+++ src/main/java/fr/communaywen/core/commands/LinkCommand.java
@@ -1,18 +1,10 @@
 package fr.communaywen.core.commands;
 
 import fr.communaywen.core.utils.LinkerAPI;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BookMeta;
-
-import java.lang.foreign.Linker;
-import java.util.List;
-
 
 public class LinkCommand implements CommandExecutor {
 
