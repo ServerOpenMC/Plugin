ee987b078fda8f038c232c46c7cd9c9d166bbba7
misieur <misieur.off@gmail.com>
2024-07-06 17:07:26 +0200

    Merge remote-tracking branch 'origin/main'

--- src/main/java/fr/communaywen/core/AywenCraftPlugin.java
+++ src/main/java/fr/communaywen/core/AywenCraftPlugin.java
@@ -103,7 +103,7 @@ public final class AywenCraftPlugin extends JavaPlugin {
         this.getCommand("feed").setExecutor(new FeedCommand(this));
         getServer().getPluginManager().registerEvents(new AntiTrampling(),this);
         getServer().getPluginManager().registerEvents(new RTPWand(this), this);
-        this.getCommand("tprequest").setExecutor(new CommandTPA());
+        this.getCommand("tpa").setExecutor(new CommandTPA());
         this.getCommand("tpaccept").setExecutor(new CommandTpaccept(this));
         this.getCommand("tpdeny").setExecutor(new CommandTpdeny());
 
