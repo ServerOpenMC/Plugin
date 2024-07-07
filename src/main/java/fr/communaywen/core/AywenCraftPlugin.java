f78a964c166e1408a1e3ca156709131947093aac
ddemile <emilienhoarau10@gmail.com>
2024-07-07 20:44:51 +0400

    feat: implement revxrsal command system for all commands

--- src/main/java/fr/communaywen/core/AywenCraftPlugin.java
+++ src/main/java/fr/communaywen/core/AywenCraftPlugin.java
@@ -6,10 +6,10 @@ import fr.communaywen.core.staff.players.PlayersCommand;
 import fr.communaywen.core.teams.*;
 import fr.communaywen.core.utils.*;
 
-import fr.communaywen.core.tpa.CommandTPA;
-import fr.communaywen.core.tpa.CommandTpaccept;
-import fr.communaywen.core.tpa.CommandTpcancel;
-import fr.communaywen.core.tpa.CommandTpdeny;
+import fr.communaywen.core.tpa.TPACommand;
+import fr.communaywen.core.tpa.TpacceptCommand;
+import fr.communaywen.core.tpa.TpcancelCommand;
+import fr.communaywen.core.tpa.TpdenyCommand;
 
 import fr.communaywen.core.economy.EconomyManager;
 import dev.xernas.menulib.MenuLib;
@@ -21,23 +21,16 @@ import fr.communaywen.core.listeners.FreezeListener;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.luckperms.api.LuckPerms;
 import org.bukkit.Bukkit;
-import org.bukkit.command.PluginCommand;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import revxrsal.commands.bukkit.BukkitCommandHandler;
 
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
 
 import java.io.File;
 
@@ -123,33 +116,11 @@ public final class AywenCraftPlugin extends JavaPlugin {
         this.handler.accept(interactiveHelpMenu);
 
         this.handler.register(new SpawnCommand(this), new VersionCommand(this), new RulesCommand(bookConfig),
-                new TeamCommand(), new MoneyCommand(this.economyManager));
+                new TeamCommand(), new MoneyCommand(this.economyManager), new ProutCommand(), new FeedCommand(this),
+                new TPACommand(this), new TpacceptCommand(), new TpcancelCommand(), new TpdenyCommand(), new CreditCommand(),
+                new ExplodeRandomCommand(), new LinkCommand(linkerAPI), new ManualLinkCommand(linkerAPI), new RTPCommand(this),
+                new FreezeCommand(), new PlayersCommand());
 
-        this.getCommand("link").setExecutor(new LinkCommand(linkerAPI));
-        this.getCommand("manuallink").setExecutor(new ManualLinkCommand(linkerAPI));
-        this.getCommand("credit").setExecutor(new CreditCommand());
-        this.getCommand("exploderandom").setExecutor(new ExplodeRandomCommand());
-        this.getCommand("rtp").setExecutor(new RTPCommand(this));
-        this.getCommand("feed").setExecutor(new FeedCommand(this));
-
-        this.getCommand("tpa").setExecutor(new CommandTPA(this));
-        this.getCommand("tpaccept").setExecutor(new CommandTpaccept());
-        this.getCommand("tpdeny").setExecutor(new CommandTpdeny());
-
-        this.getCommand("freeze").setExecutor(new FreezeCommand());
-        this.getCommand("players").setExecutor(new PlayersCommand());
-
-
-
-        final @Nullable PluginCommand proutCommand = super.getCommand("prout");
-        if (proutCommand != null)
-            proutCommand.setExecutor(new ProutCommand());
-
-        this.getCommand("tpa").setExecutor(new CommandTPA(this));
-        this.getCommand("tpa").setTabCompleter(new CommandTPA(this));
-        this.getCommand("tpaccept").setExecutor(new CommandTpaccept());
-        this.getCommand("tpdeny").setExecutor(new CommandTpdeny());
-        this.getCommand("tpcancel").setExecutor(new CommandTpcancel());
         /*  --------  */
 
         /* LISTENERS */
--- src/main/java/fr/communaywen/core/staff/players/PlayersCommand.java
+++ src/main/java/fr/communaywen/core/staff/players/PlayersCommand.java
@@ -2,23 +2,16 @@ package fr.communaywen.core.staff.players;
 
 import fr.communaywen.core.utils.PlayersMenuUtils;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import revxrsal.commands.annotation.Command;
+import revxrsal.commands.annotation.Description;
+import revxrsal.commands.bukkit.annotation.CommandPermission;
 
-public class PlayersCommand implements CommandExecutor {
-	 @Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		 if (sender instanceof Player player) {
-			if (args.length != 0) {
-				player.sendMessage(ChatColor.RED + "La commande est " + ChatColor.BLUE + "/players");
-				return false;
-			}
-			PlayersMenuUtils.openPlayersMenu(player);
-			return true;
-		 } else {
-			 return false;
-		 }
+public class PlayersCommand {
+	@Command("players")
+	@Description("Ouvre le menu des joueurs")
+	@CommandPermission("openmc.staff.players")
+	public void onCommand(Player player) {
+		 PlayersMenuUtils.openPlayersMenu(player);
 	}
 }
