4aae45eacc55061ff8b49156f4c47389b1b52724
Youn Tanguy <103602438+younig@users.noreply.github.com>
2024-07-07 10:23:47 +0200

    [feature] Ajoute un title à la première connection
    Ajout de WelcomeMessage qui affiche un title à la
    première connection du joueur. Ajout du fichier
    WelcomeMessageCOnfig.yml pour configurer ce title.
    Ajout de l'event dans AywenCraftPlugin.

---
+++ src/main/java/fr/communaywen/core/listeners/WelcomeMessage.java
@@ -0,0 +1,32 @@
+package fr.communaywen.core.listeners;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+// show the title "Merci de ne pas grief" at the player's first connection
+
+public class WelcomeMessage implements Listener {
+
+    private final FileConfiguration welcomeMessageConfig;
+
+    public WelcomeMessage(FileConfiguration welcomeMessageConfig) {
+        this.welcomeMessageConfig = welcomeMessageConfig;
+    }
+
+    @EventHandler
+    public void OnPlayerJoin(PlayerJoinEvent event) {
+        final Player player = event.getPlayer();
+
+        if (player.hasPlayedBefore()) {
+            player.sendTitle(welcomeMessageConfig.getString("title"),
+                    welcomeMessageConfig.getString("subtitle"),
+                    welcomeMessageConfig.getInt("fadeIn"),
+                    welcomeMessageConfig.getInt("stay"),
+                    welcomeMessageConfig.getInt("fadeOut"));
+        }
+    }
+
+}
