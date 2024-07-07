5cfd5803faf084ea310b38d601c8c3570e7f855d
TheR0001 <164377596+TheR0001@users.noreply.github.com>
2024-07-07 15:32:58 +0200

    Update MoneyCommand.java

--- src/main/java/fr/communaywen/core/commands/MoneyCommand.java
+++ src/main/java/fr/communaywen/core/commands/MoneyCommand.java
@@ -2,18 +2,14 @@ package fr.communaywen.core.commands;
 
 import fr.communaywen.core.economy.EconomyManager;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
+public class MoneyCommand implements CommandExecutor {
 
-public class MoneyCommand implements CommandExecutor, TabCompleter {
     private final EconomyManager economyManager;
 
     public MoneyCommand(EconomyManager economyManager) {
@@ -22,58 +18,55 @@ public class MoneyCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage("This command can only be run by a player.");
-            return true;
-        }
-
-        Player player = (Player) sender;
-
         if (args.length == 0) {
-            player.sendMessage("Balance: " + economyManager.getBalance(player));
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                double balance = economyManager.getBalance(player);
+                player.sendMessage(ChatColor.GOLD + "------------------------------");
+                player.sendMessage(ChatColor.GREEN + "Votre solde actuel est de : " + ChatColor.YELLOW + balance + " " + ChatColor.GREEN + "monnaie.");
+                player.sendMessage(ChatColor.GOLD + "------------------------------");
+            } else {
+                sender.sendMessage(ChatColor.RED + "Seuls les joueurs peuvent exécuter cette commande.");
+            }
             return true;
         }
 
         if (args.length == 3 && args[0].equalsIgnoreCase("transfer")) {
-            Player target = Bukkit.getPlayer(args[1]);
-            double amount;
-
-            try {
-                amount = Double.parseDouble(args[2]);
-            } catch (NumberFormatException e) {
-                player.sendMessage("Invalid amount.");
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(ChatColor.RED + "Seuls les joueurs peuvent exécuter cette commande.");
                 return true;
             }
 
-            if (target != null && economyManager.transferBalance(player, target, amount)) {
-                player.sendMessage("Transferred " + amount + " to " + target.getName());
-                target.sendMessage("Received " + amount + " from " + player.getName());
-            } else {
-                player.sendMessage("Transfer failed.");
+            Player player = (Player) sender;
+            Player target = Bukkit.getPlayer(args[1]);
+            if (target == null) {
+                player.sendMessage(ChatColor.RED + "Le joueur spécifié est introuvable.");
+                return true;
             }
 
+            try {
+                double amount = Double.parseDouble(args[2]);
+                if (amount <= 0) {
+                    player.sendMessage(ChatColor.RED + "Le montant doit être positif.");
+                    return true;
+                }
+
+                if (economyManager.getBalance(player) < amount) {
+                    player.sendMessage(ChatColor.RED + "Vous n'avez pas assez de monnaie.");
+                    return true;
+                }
+
+                economyManager.withdraw(player, amount);
+                economyManager.deposit(target, amount);
+                player.sendMessage(ChatColor.GREEN + "Vous avez transféré " + ChatColor.YELLOW + amount + " " + ChatColor.GREEN + "monnaie à " + ChatColor.YELLOW + target.getName() + ChatColor.GREEN + ".");
+                target.sendMessage(ChatColor.GREEN + "Vous avez reçu " + ChatColor.YELLOW + amount + " " + ChatColor.GREEN + "monnaie de la part de " + ChatColor.YELLOW + player.getName() + ChatColor.GREEN + ".");
+            } catch (NumberFormatException e) {
+                player.sendMessage(ChatColor.RED + "Le montant spécifié n'est pas valide.");
+            }
             return true;
         }
 
-        player.sendMessage("Usage: /money [transfer <player> <amount>]");
+        sender.sendMessage(ChatColor.RED + "Usage: /money [transfer <joueur> <montant>]");
         return true;
     }
-    @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
-
-        List<String> tab = new ArrayList<>();
-
-        if(args.length == 1) {
-            tab.add("transfer");
-        } else if(args.length == 2) {
-          for(Player players : Bukkit.getOnlinePlayers()) {
-              tab.add(players.getName());
-          }
-        } else if(args.length == 3) {
-            tab.add("<amout>");
-        }
-
-        return tab;
-    }
-
 }
