f734ce05e020e62b16bcac979a0fd6d97670ef7d
misieur <153451816+misieur@users.noreply.github.com>
2024-07-08 17:48:10 +0200

    Update TpacceptCommand.java

--- src/main/java/fr/communaywen/core/tpa/TpacceptCommand.java
+++ src/main/java/fr/communaywen/core/tpa/TpacceptCommand.java
@@ -21,7 +21,7 @@ public class TpacceptCommand {
         }
         tpQueue.TPA_REQUESTS.remove(player);
         player.sendMessage(tpaplayer.getName()+" va être téléporté à vous dans 3 secondes");
-        tpaplayer.sendTitle("§0","Téléportation à "+player.getName()+" dans 3 secondes...",20,10,10);
+        tpaplayer.sendTitle("§0","Téléportation à "+player.getName()+" dans 3 secondes...",20,10,10);
         new BukkitRunnable() {
             @Override
             public void run() {
