39ed14ac26c78357e928f1ea8b8c4ad02c7231f8
MathiasDPX <mathias@dupeux.net>
2024-08-01 17:08:31 +0200

    Add credit

--- src/main/java/fr/communaywen/core/commands/MoneyCommand.java
+++ src/main/java/fr/communaywen/core/commands/MoneyCommand.java
@@ -20,7 +20,7 @@ import revxrsal.commands.help.CommandHelp;
 @Command("money")
 @Description("Transférer de l'argent entre joueurs")
 @Feature("Money")
-@Credit({"Axeno", "Koneiii", "TheR0001"})
+@Credit({"Axeno", "Koneiii", "TheR0001", "Gyro3630"})
 public class MoneyCommand {
     private final EconomyManager economyManager;
     AywenCraftPlugin plugin;
