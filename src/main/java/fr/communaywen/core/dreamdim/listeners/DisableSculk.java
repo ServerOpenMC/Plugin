9bba874f5cac3663069af2719350115c6dd3d252
MathiasDPX <mathias@dupeux.net>
2024-08-04 18:27:42 +0200

    Disable drop

--- src/main/java/fr/communaywen/core/dreamdim/listeners/DisableSculk.java
+++ src/main/java/fr/communaywen/core/dreamdim/listeners/DisableSculk.java
@@ -5,13 +5,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
-public class DisableSculkExp implements Listener {
+public class DisableSculk implements Listener {
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         if (!event.getPlayer().getWorld().getName().equals("dreamworld")) { return; }
 
         if (event.getBlock().getType() == Material.SCULK) {
             event.setExpToDrop(0);
+            event.setDropItems(false);
         }
     }
 }
