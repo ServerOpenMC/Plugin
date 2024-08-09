b2d6e7f480bd087828ab30aec27c406b304363c3
MathiasDPX <mathias@dupeux.net>
2024-08-09 15:54:42 +0200

    Disable debris placement

--- src/main/java/fr/communaywen/core/dreamdim/listeners/DebrisHandler.java
+++ src/main/java/fr/communaywen/core/dreamdim/listeners/DebrisHandler.java
@@ -8,12 +8,23 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 
 public class DebrisHandler implements Listener {
     AdvancementRegister register;
 
     public DebrisHandler(AdvancementRegister register) {
         this.register = register;
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Block block = event.getBlock();
+        Player player = event.getPlayer();
+        if (!player.getWorld().getName().equals("dreamworld")) { return; }
+        if (!block.getType().equals(Material.ANCIENT_DEBRIS)) { return; }
+        event.setBuild(false);
+        event.setCancelled(true);
     }
 
     @EventHandler
