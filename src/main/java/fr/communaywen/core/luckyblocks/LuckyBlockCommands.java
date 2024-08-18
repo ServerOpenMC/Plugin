72415a7e3af0fbfad516eaa137beed853d0953da
JeanTheBest <jeanjankov@gmail.com>
2024-08-18 21:13:59 +0200

    Ajout de a structure du lucky block

--- src/main/java/fr/communaywen/core/luckyblocks/LuckyBlockCommands.java
+++ src/main/java/fr/communaywen/core/luckyblocks/LuckyBlockCommands.java
@@ -3,7 +3,6 @@ package fr.communaywen.core.luckyblocks;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 public class LuckyBlockCommands implements CommandExecutor {
     @Override
