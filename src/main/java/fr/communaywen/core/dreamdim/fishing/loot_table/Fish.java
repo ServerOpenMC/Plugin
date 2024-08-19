97898402e5099e4b34cd8983ed52b24dd5da132f
MathiasDPX <mathias@dupeux.net>
2024-08-20 01:59:10 +0200

    Add fishs to fishing

---
+++ src/main/java/fr/communaywen/core/dreamdim/fishing/loot_table/Fish.java
@@ -0,0 +1,29 @@
+package fr.communaywen.core.dreamdim.fishing.loot_table;
+
+import fr.communaywen.core.dreamdim.fishing.LootCategory;
+import fr.communaywen.core.dreamdim.fishing.LootStack;
+import fr.communaywen.core.dreamdim.fishing.loot_table.fish.MoonFish;
+import fr.communaywen.core.dreamdim.fishing.loot_table.junk.Bowl;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+
+public class Fish extends LootCategory {
+
+    @Override
+    public @NotNull Set<LootStack> getLoots() {
+        return Set.of( //Ajoutez en haut pour éviter les conflits, merci
+            new MoonFish()
+        );
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return "Fish";
+    }
+
+    @Override
+    public @NotNull Double getChance() {
+        return 0.6;
+    }
+}
---
+++ src/main/java/fr/communaywen/core/dreamdim/fishing/loot_table/fish/MoonFish.java
@@ -0,0 +1,35 @@
+package fr.communaywen.core.dreamdim.fishing.loot_table.fish;
+
+import dev.lone.itemsadder.api.CustomStack;
+import fr.communaywen.core.dreamdim.fishing.LootStack;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+public class MoonFish extends LootStack {
+    CustomStack moon_fish;
+
+    public MoonFish() {
+        moon_fish = CustomStack.getInstance("aywen:moon_fish");
+    }
+
+    @Override
+    public @NotNull Double getChance(int LureLevel) {
+        return 1.0;
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return "Poisson-lune";
+    }
+
+    @Override
+    public @NotNull ItemStack toItemStack(@NotNull Player player) {
+        return moon_fish.getItemStack();
+    }
+
+    @Override
+    public void onCatched(@NotNull Player player) {
+
+    }
+}
