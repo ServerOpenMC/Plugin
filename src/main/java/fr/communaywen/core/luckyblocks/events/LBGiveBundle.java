8c73520243ddf9bfd6fe3a92902799b5dbe067bd
Fnafgameur <chara60etdashi@gmail.com>
2024-08-25 18:54:16 +0200

    Ajout de l'event "Spawn Shulker" Changement d'un event de test pour "Give Bundle"

---
+++ src/main/java/fr/communaywen/core/luckyblocks/events/LBGiveBundle.java
@@ -0,0 +1,24 @@
+package fr.communaywen.core.luckyblocks.events;
+
+import fr.communaywen.core.credit.Credit;
+import fr.communaywen.core.credit.Feature;
+import fr.communaywen.core.luckyblocks.objects.LuckyBlockEvent;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+@Feature("Lucky Blocks")
+@Credit("Fnafgameur")
+public class LBGiveBundle extends LuckyBlockEvent {
+
+    public LBGiveBundle() {
+        super("Rangement insolite", 0.7f);
+    }
+
+    public void onOpen(Player player, Block block) {
+
+        ItemStack bundle = new ItemStack(Material.BUNDLE);
+        block.getWorld().dropItemNaturally(block.getLocation(), bundle);
+    }
+}
--- src/main/java/fr/communaywen/core/luckyblocks/managers/LuckyBlockManager.java
+++ src/main/java/fr/communaywen/core/luckyblocks/managers/LuckyBlockManager.java
@@ -2,7 +2,8 @@ package fr.communaywen.core.luckyblocks.managers;
 
 import fr.communaywen.core.credit.Credit;
 import fr.communaywen.core.credit.Feature;
-import fr.communaywen.core.luckyblocks.events.LBExplode;
+import fr.communaywen.core.luckyblocks.events.LBGiveBundle;
+import fr.communaywen.core.luckyblocks.events.LBSpawnShulker;
 import fr.communaywen.core.luckyblocks.objects.LuckyBlockEvent;
 
 import java.util.ArrayList;
@@ -16,9 +17,14 @@ public class LuckyBlockManager {
     private final ArrayList<LuckyBlockEvent> lbEvents = new ArrayList<>();
 
     public LuckyBlockManager() {
-        lbEvents.add(new LBExplode());
+        lbEvents.add(new LBGiveBundle());
+        lbEvents.add(new LBSpawnShulker());
     }
 
+    /**
+     * Permet de récupérer un événement aléatoire parmi la liste "lbEvents"
+     * @return un événement aléatoire
+     */
     public LuckyBlockEvent getRandomEvent() {
         return lbEvents.get(random.nextInt(lbEvents.size()));
     }
