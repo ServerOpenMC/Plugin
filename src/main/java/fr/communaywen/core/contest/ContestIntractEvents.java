b2a5bb09b31067e580e45b8c4102a58df532d6b1
iambibi <lethiebaut2@gmail.com>
2024-08-29 21:27:06 +0200

    Add in ContestIntractEvents - Mise d'un son lors de l'ouverture de la Borne

--- src/main/java/fr/communaywen/core/contest/ContestIntractEvents.java
+++ src/main/java/fr/communaywen/core/contest/ContestIntractEvents.java
@@ -1,6 +1,7 @@
 package fr.communaywen.core.contest;
 
 import dev.lone.itemsadder.api.Events.FurnitureInteractEvent;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,6 +13,7 @@ public class ContestIntractEvents implements Listener {
     @EventHandler
     private void onFurnitureInteractEvent(FurnitureInteractEvent furniture) {
         if (Objects.equals(furniture.getNamespacedID(), "contest:borne")) {
+            furniture.getPlayer().playSound(furniture.getPlayer().getLocation(), Sound.BLOCK_BARREL_OPEN, 1.0F, 0.7F);
             Bukkit.dispatchCommand(furniture.getPlayer(), "aywencraftplugin:contest");
         }
     }
