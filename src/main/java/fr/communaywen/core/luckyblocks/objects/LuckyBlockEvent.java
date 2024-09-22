605a62ca81290a0dd639a308b4378a55b7c3112f
= <=>
2024-09-22 14:00:01 +0200

    Removed useless comments

--- src/main/java/fr/communaywen/core/luckyblocks/objects/LuckyBlockEvent.java
+++ src/main/java/fr/communaywen/core/luckyblocks/objects/LuckyBlockEvent.java
@@ -41,15 +41,10 @@ public class LuckyBlockEvent {
     public void onOpen(Player player, Block block) {
         Prefix prefix = eventType.getPrefix();
         ChatColor color = eventType.getColor();
-
-        // Affichage du nom de l'event pendant 1 seconde en subtitle
-        player.sendTitle("", color + "§l" + name, 8, 20, 8);
-
-        // Affichage de la description de l'event dans le chat
-        MessageManager.sendMessageType(player, description, prefix, MessageType.INFO, false);
-
-        // Joue un son aléatoire en fonction du type de l'event
         Sound soundToPlay = EventType.getRandomSoundByType(eventType);
+
+        player.sendTitle("", color + "§l" + name, 8, 20, 8);
+        MessageManager.sendMessageType(player, description, prefix, MessageType.INFO, false);
         block.getWorld().playSound(block.getLocation(), soundToPlay , 1, 1);
     }
 }
