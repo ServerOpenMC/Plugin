d147a29ea46fdc2b1905bbf87a53ca6b6cd536ef
= <=>
2024-09-28 12:14:22 +0200

    Fixed another dupe bug with breaking speed

--- src/main/java/fr/communaywen/core/luckyblocks/listeners/LBBlockBreakListener.java
+++ src/main/java/fr/communaywen/core/luckyblocks/listeners/LBBlockBreakListener.java
@@ -1,18 +1,22 @@
 package fr.communaywen.core.luckyblocks.listeners;
 
 import dev.lone.itemsadder.api.CustomBlock;
+import fr.communaywen.core.AywenCraftPlugin;
 import fr.communaywen.core.credit.Credit;
 import fr.communaywen.core.credit.Feature;
 import fr.communaywen.core.luckyblocks.managers.LuckyBlockManager;
 import fr.communaywen.core.luckyblocks.utils.LBUtils;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 
 @Feature("Lucky Blocks")
 @Credit("Fnafgameur")
@@ -24,7 +28,7 @@ public class LBBlockBreakListener implements Listener {
         this.luckyBlockManager = luckyBlockManager;
     }
 
-    @EventHandler
+    @EventHandler()
     public void onBreak(BlockBreakEvent event) {
 
         Player player = event.getPlayer();
@@ -44,6 +48,14 @@ public class LBBlockBreakListener implements Listener {
         }
 
         Block block = event.getBlock();
+        float blockHardness = block.getType().getHardness();
+        float breakSpeed = block.getBreakSpeed(player);
+
+        if (breakSpeed > blockHardness) {
+            event.setCancelled(true);
+            player.sendMessage("§cVous ne pouvez pas casser ce bloc aussi rapidement. (merci ItemsAdder)");
+            return;
+        }
 
         if (!LBUtils.canDestroyBlockInClaim(player, block)) {
             event.setCancelled(true);
@@ -60,6 +72,7 @@ public class LBBlockBreakListener implements Listener {
             return;
         }
 
+        block.setType(Material.AIR);
         luckyBlockManager.getRandomEvent().onOpen(player, block);
     }
 }
