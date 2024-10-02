b0ca2ff747558d894ebcf60d89014b1dbc5af6de
ri1_ <contact@notri1.fr>
2024-10-02 18:29:45 +0200

    [Fix] Custom skulls were not working
    Signed-off-by: ri1_ <contact@notri1.fr>

--- src/main/java/fr/communaywen/core/utils/Skull.java
+++ src/main/java/fr/communaywen/core/utils/Skull.java
@@ -1,14 +1,15 @@
 package fr.communaywen.core.utils;
 
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
-import com.mojang.authlib.properties.PropertyMap;
 import org.apache.commons.codec.binary.Base64;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.profile.PlayerProfile;
+import org.bukkit.profile.PlayerTextures;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.UUID;
 
 /**
@@ -61,27 +62,7 @@ public enum Skull {
         this.id = id;
     }
 
-    /**
-     * Return a skull that has a custom texture specified by url.
-     *
-     * @param url skin url
-     * @return itemstack
-     */
-    public static ItemStack getCustomSkull(String url) {
-        GameProfile profile = new GameProfile(UUID.randomUUID(), "reaper");
-        PropertyMap propertyMap = profile.getProperties();
-        if (propertyMap == null) {
-            throw new IllegalStateException("Profile doesn't contain a property map");
-        }
-        byte[] encodedData = base64.encode(String.format("{textures:{SKIN:{url:\"%s\"}}}", url).getBytes());
-        propertyMap.put("textures", new Property("textures", new String(encodedData)));
-        ItemStack head = new ItemStack(Material.PLAYER_HEAD, 1, (short) 3);
-        ItemMeta headMeta = head.getItemMeta();
-        Class<?> headMetaClass = headMeta.getClass();
-        Reflections.getField(headMetaClass, "profile", GameProfile.class).set(headMeta, profile);
-        head.setItemMeta(headMeta);
-        return head;
-    }
+
 
     /**
      * Return a skull of a player.
@@ -113,6 +94,39 @@ public enum Skull {
      */
     public ItemStack getSkull() {
         return getPlayerSkull(id);
+    }
+
+    private static final UUID RANDOM_UUID = UUID.fromString("92864445-51c5-4c3b-9039-517c9927d1b4"); // We reuse the same "random" UUID all the time
+
+    private static PlayerProfile getProfile(String url) {
+        PlayerProfile profile = Bukkit.createPlayerProfile(RANDOM_UUID); // Get a new player profile
+        PlayerTextures textures = profile.getTextures();
+        URL urlObject;
+        try {
+            urlObject = new URL(url); // The URL to the skin, for example: https://textures.minecraft.net/texture/18813764b2abc94ec3c3bc67b9147c21be850cdf996679703157f4555997ea63a
+        } catch (MalformedURLException exception) {
+            throw new RuntimeException("Invalid URL", exception);
+        }
+        textures.setSkin(urlObject); // Set the skin of the player profile to the URL
+        profile.setTextures(textures); // Set the textures back to the profile
+        return profile;
+    }
+
+
+    /**
+     * Return a skull that has a custom texture specified by url.
+     *
+     * @param url skin url
+     * @return itemstack
+     */
+    public static ItemStack getCustomSkull(String url) {
+        PlayerProfile profile = getProfile(url);
+        System.out.println(profile.getTextures().getSkin().toString());
+        ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta meta = (SkullMeta) head.getItemMeta();
+        meta.setOwnerProfile(profile); // Set the owning player of the head to the player profile
+        head.setItemMeta(meta);
+        return head; // Return the player head
     }
 
 }
