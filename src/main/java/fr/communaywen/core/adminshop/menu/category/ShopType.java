4ee90151749a577459f5d6534e9954549b00aea8
AxenoDev <tbavart@gmail.com>
2024-10-06 00:37:23 +0200

    [Added] Added back button into adminshop

--- src/main/java/fr/communaywen/core/adminshop/menu/category/ShopType.java
+++ src/main/java/fr/communaywen/core/adminshop/menu/category/ShopType.java
@@ -1,10 +1,5 @@
 package fr.communaywen.core.adminshop.menu.category;
 
-import fr.communaywen.core.credit.Credit;
-import fr.communaywen.core.credit.Feature;
-
-@Credit("Axeno")
-@Feature("AdminShop")
 public enum ShopType {
     SELL,
     BUY,
